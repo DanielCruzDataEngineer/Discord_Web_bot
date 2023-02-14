fd6c0f89506d4a52a5cb7ef0c6135003a91b1641
DanielCruzDataEngineer <danielcruz.alu.lmb@gmail.com>
2023-02-14 20:12:07 -0300

    Adicionando arquivos modificados

--- count_exec.py
+++ count_exec.py
@@ -1,3 +1,11 @@
-import openpyxl
-import os
+import git
 
+# inicializa o repositório git
+repo = git.Repo('..\Discord_Web_bot')
+
+# faz o add dos arquivos modificados
+repo.git.add(update=True)
+
+# cria um commit com a mensagem informando qual comando foi executado
+commit_message = "Adicionando arquivos modificados"
+repo.index.commit(commit_message)
